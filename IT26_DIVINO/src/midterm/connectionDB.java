3db205df9ef7c8413c0c1c8da15af6021b4cd75c
meya <michaelladrenojo>
2026-04-09 09:12:23 +0800

    added function for connectionDB

--- IT26_DIVINO/src/midterm/connectionDB.java
+++ IT26_DIVINO/src/midterm/connectionDB.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+
 package midterm;
 
-/**
- *
- * @author Acer
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class connectionDB {
     
 }
